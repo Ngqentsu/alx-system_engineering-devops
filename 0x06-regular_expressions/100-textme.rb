7a07b7dc9387403e140f8882d99bd93e03485c11
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-08-01 15:03:00 -0700

    run some statistics on the TextMe app text messages transactions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/\[(from:|to:|flags:)([^\]]+)\]/).map { |label, value| value }.join(',')
+puts ARGV[0].scan(/\[(from:|to:|flags:)([^\]]+)\]/).map { |sender_or_receiver, flag| flag }.join(',')
