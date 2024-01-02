96cdd69d6f49f725c1ebfb892fdc516e32d52457
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2024-01-02 06:19:10 -0800

    Python script to export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Python script that uses REST API,
+for a given employee ID, returns information
+about his/her TODO list progress and exports data in CSV format.
+"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+
+    employee_id = int(sys.argv[1])
+    todos_response = requests.get(todos_url, params={"userId": employee_id})
+    user_response = requests.get(users_url + f'/{employee_id}')
+
+    todos = todos_response.json()
+    user = user_response.json()
+
+    user_id = user.get('id')
+    username = user.get('username')
+
+    csv_filename = f'{user_id}.csv'
+    with open(csv_filename, 'w', newline='') as csvfile:
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+
+        for task in todos:
+            csv_writer.writerow([user_id, username,
+                                 task["completed"], task["title"]])
