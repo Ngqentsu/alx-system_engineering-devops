ced5a5ae4bf2830b911d4552f16dcb1a34b4b1b1
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2024-06-13 12:38:55 -0700

    Fix for open file limits

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,13 @@
 # Puppet to fix limits on open files
 
 exec { 'set soft nofile limit':
-  command => '/bin/grep -q "holberton soft nofile 1500" /etc/security/limits.conf || echo "holberton soft nofile 1500" >> /etc/security/limits.conf',
+  command => '/bin/sed -i "holberton soft nofile 4/holberton hard nofile 1500/" /etc/security/limits.conf'
 }
 
 exec { 'set hard nofile limit':
-  command => '/bin/grep -q "holberton hard nofile 1500" /etc/security/limits.conf || echo "holberton hard nofile 1500" >> /etc/security/limits.conf',
+  command => '/bin/sed -i "holberton hard nofile 5/holberton hard nofile 1500/" /etc/security/limits.conf'
 }
 
 exec { 'set file limits':
-  command => '/bin/true',
-  require => [ Exec['set soft nofile limit'], Exec['set hard nofile limit'] ],
+  command => '/usr/sbin/sysctl -p'
 }
